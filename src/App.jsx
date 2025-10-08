0e294f55504b10491217a352c5bb2a9349dae675
kashifshabbir2004-lab <kashifshabbir2004@gmail.com>
2025-10-07 18:57:25 -0700

    New changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import User from './User';
-
-function App() {
-  //const [count, setCount] = useState(0);
-  const [Counter, setCounter] = useState(0);
-
-  return (
-    <>
-      <h2>Counter Val: {Counter}</h2>
-      <User />
-      <button onClick={() => setCounter(Counter + 1)}>
-        Increase Counter Value
-      </button>
-    </>
-  );
+function Apple(){
+  return <div>
+    <h1>Hello React</h1>
+  </div>
 }
 
-export default App;
+export default Apple;
