bae2881a3f46b18be36c919aa3ad09bcb8f84c89
kashifshabbir2004-lab <kashifshabbir2004@gmail.com>
2025-11-06 23:24:57 +0500

    previous tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import User from './User';
+import Header from "./Header";
 
-function App() {
-  //const [count, setCount] = useState(0);
-  const [Counter, setCounter] = useState(0);
-
-  return (
-    <>
-      <h2>Counter Val: {Counter}</h2>
-      <User />
-      <button onClick={() => setCounter(Counter + 1)}>
-        Increase Counter Value
-      </button>
-    </>
-  );
+function Apple(){
+  return(
+    <div>
+      <Header />
+      <h1>Hello React</h1>
+      <h2>Code Step by Step 2</h2>
+    </div>
+  ) 
 }
 
-export default App;
+export default Apple;
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return <div>
+        <h1>Header File</h1>
+    </div>
+}
+
+export default Header;
