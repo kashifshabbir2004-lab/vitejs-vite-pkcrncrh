621b7dd28b5e7b8decf3bb7caba0ac93acce8d76
kashifshabbir2004-lab <kashifshabbir2004@gmail.com>
2026-01-26 19:01:58 +0500

    new change in tutorial part 15

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -9,7 +9,7 @@ const Counter=()=>{
     return(                
         <>
             <h1>Count : {count}</h1>
-            <h2>Reverse Count: {rcount}</h2>
+            <h2>R Count: {rcount}</h2>
             <button onClick={()=>addCount(count)}>Counter</button>
             {/* <button onClick={()=>setCount(count+1)}>Counter</button> */}
             <button onClick={()=>setRCount(rcount - 1)}>Reverse Counter</button>
